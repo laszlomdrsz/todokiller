d5415dbdd2ac32eacf67da74b7cee36117a8e71a
laszlomdrsz <laszlomdrsz@gmail.com>
2018-02-12 10:15:45 +0100

    node server

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const http = require('http');
+const app = express();
+
+
+const index = path.join(__dirname, 'dist', 'index.html');
+
+app.use(express.static(path.join(__dirname, 'dist')));
+
+app.get('**', (req, res, next) => {
+    if (req.session.user) {   
+        res.sendFile(index);
+    }
+});
+
+
+
+
+//Set Port
+const port = process.env.PORT || '51000';
+app.set('port', port);
+
+const server = http.createServer(app);
+
+server.listen(port, () => console.log(`Running on localhost:${port}`));
